d305f7b2ad6b2fdd3f3339fae82cb4e3e7a1a636
Jeetze <gjazz@163.com>
2013-08-30 13:09:28 +0800

    Initial sameple_app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c74c4a730ca0a526096260417f22e84cbb4c4047cc3a0363637ba80d40e1a59207a87c4e206fbfca685e583b61ad2a816a184f4ac2e5b7bac82e0993efd90fd1'
